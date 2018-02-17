9d865ec96e4245b10b6a7fae34143822e44aa231
severjason <ssever2010@gmail.com>
2018-02-17 23:44:17 +0200

    Finished step 2: Build a static version fo the app

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,9 @@ class TimersDashboard extends React.Component {
             <div className='ui three column centered grid'>
                 <div className='column'>
                     <EditableTimerList />
-                    {/*<ToggleableTimerForm
+                    <ToggleableTimerForm
                         isOpen={true}
-                        />*/}
+                        />
                 </div>
             </div>
         )
@@ -63,7 +63,78 @@ class TimerForm extends React.Component {
     render() {
         const submitText = this.props.title ? 'Update' : 'Create';
 
-        return ()
+        return (
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='ui form'>
+                        <div className='field'>
+                            <label>Title</label>
+                            <input type='text' defaultValue={this.props.title}/>
+                        </div>
+                        <div className='ui two bottom attached buttons'>
+                            <button className='ui basic blue button'>
+                                {submitText}
+                            </button>
+                            <button className='ui basic red button'>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class ToggleableTimerForm extends React.Component {
+    render() {
+        if (this.props.isOpen) {
+            return (
+                <TimerForm/>
+            );
+        } else {
+            return (
+                <div className='ui basic content center aligned segment'>
+                    <button className='ui basic button icon'>
+                        <i className='plus icon' />
+                    </button>
+                </div>
+            )
+        }
+    }
+}
+
+class Timer extends React.Component {
+    render() {
+        const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+        return (
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='header'>
+                        {this.props.title}
+                    </div>
+                    <div className='meta'>
+                        {this.props.project}
+                    </div>
+                    <div className='center aligned description'>
+                        <h2>
+                            {elapsedString}
+                        </h2>
+                    </div>
+                    <div className='extra content'>
+                        <span className='right floated edit icon'>
+                            <i className='edit icon' />
+                        </span>
+                        <span className='right floated edit icon'>
+                            <i className='trash icon' />
+                        </span>
+                    </div>
+                </div>
+                <div className='ui bottom attached blue basic button'>
+                    Start
+                </div>
+            </div>
+        )
     }
 }
 
