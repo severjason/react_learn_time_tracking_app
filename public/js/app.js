ec90c30c3bae5cd6e9516660c2edb703be1cd6b5
severjason <ssever2010@gmail.com>
2018-02-17 15:25:58 +0200

    Start time tracking app

---
+++ public/js/app.js
@@ -0,0 +1,75 @@
+class TimersDashboard extends React.Component {
+
+    render() {
+        return (
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <EditableTimerList />
+                    {/*<ToggleableTimerForm
+                        isOpen={true}
+                        />*/}
+                </div>
+            </div>
+        )
+    }
+}
+
+class EditableTimerList extends React.Component {
+    render() {
+        return (
+            <div id='timers'>
+                <EditableTimer
+                    title='Learn React'
+                    project='Web Domination'
+                    elapsed='8986300'
+                    runningSince={null}
+                    editFormOpen={false}
+                />
+                <EditableTimer
+                    title='Learn extreme ironing'
+                    project='World Domination'
+                    elapsed='3890985'
+                    runningSince={null}
+                    editFormOpen={true}
+                />
+            </div>
+        )
+    }
+}
+
+class EditableTimer extends React.Component {
+    render() {
+        if (this.props.editFormOpen) {
+            return (
+                <TimerForm
+                    title={this.props.title}
+                    project={this.props.project}
+                />
+            );
+        } else {
+            return (
+                <Timer
+                    title={this.props.title}
+                    project={this.props.project}
+                    elapsed={this.props.elapsed}
+                    runningSince={this.props.runningSince}
+                />
+            );
+        }
+    }
+}
+
+class TimerForm extends React.Component {
+    render() {
+        const submitText = this.props.title ? 'Update' : 'Create';
+
+        return ()
+    }
+}
+
+
+ReactDOM.render(
+    <TimersDashboard/>,
+    document.getElementById('content')
+);
+
